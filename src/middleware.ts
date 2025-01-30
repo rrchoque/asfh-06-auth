f909a7211bdb3742fac6b8c0aecba158d727897a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-01-30 06:48:07 -0400

    Se agregaron mensajes de error con sweetalert2

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,30 +6,5 @@ const privateRoutes = ["/protected"];
 export const onRequest = defineMiddleware((context, next) => {
   const { url, request } = context;
 
-  const authHeaders = request.headers.get("authorization");
-
-  if (privateRoutes.includes(url.pathname)) {
-    return checkLocalAuth(authHeaders, next);
-  }
-  //console.log(context.url);
   return next();
 });
-
-const checkLocalAuth = (authHeaders: string | null, next: MiddlewareNext) => {
-  if (authHeaders) {
-    const authValue = authHeaders.split(" ").at(-1) ?? "user:pass";
-    const decodedValue = atob(authValue).split(":");
-    const [user, password] = decodedValue;
-
-    if (user === "admin" && password === "admin") {
-      return next();
-    }
-  }
-
-  return new Response("Auth Necesaria", {
-    status: 401,
-    headers: {
-      "WWW-Authenticate": 'Basic real="Secure Area"',
-    },
-  });
-};
