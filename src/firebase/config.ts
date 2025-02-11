2e24b99ffe5695bf329a96a15a2a97e7477b7ad1
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2025-02-11 08:34:08 -0400

    Correciones y eliminación de consol log

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -6,14 +6,6 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBVurJn0d2QhRun8TC1vpyU6wXqVvLv3xI",
-//   authDomain: "astro-authentication-ef65c.firebaseapp.com",
-//   projectId: "astro-authentication-ef65c",
-//   storageBucket: "astro-authentication-ef65c.firebasestorage.app",
-//   messagingSenderId: "1021142421285",
-//   appId: "1:1021142421285:web:b9dcb634fb46ba350254cc",
-// };
 
 const firebaseConfig = {
   apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
@@ -23,8 +15,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.PUBLIC_FIREBASE_APP_ID,
 };
-
-console.log({ firebaseConfig });
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
